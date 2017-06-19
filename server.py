ca831b0a02feb67c0c940bb9c58331c824af8585
Sad0range <evgeniyd96@gmail.com>
2017-06-19 21:18:37 +0300

    server changes

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+from Crypto.Cipher import AES
+from Crypto import Random
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 import sys
@@ -5,6 +7,7 @@ import socket
 from select import select
  
 SOCKET_LIST = []
+SOCKET_SESSION = []
 CLIENTS = []
 RECV_BUFFER = 4096 
 
@@ -47,13 +50,16 @@ def server():
                 try:
                     data = sock.recv(RECV_BUFFER)
                     if data:
-                        cipherrsa = PKCS1_OAEP.new(privatekey)
-                        raw = cipherrsa.decrypt(data)
-                        client_publickey = RSA.importKey(raw[10:])
-                        cipherrsa = PKCS1_OAEP.new(client_publickey)
-                        msg = registration(data)
-                        msg = cipherrsa.encrypt(client_publickey)    
-                        sock.send(msg)    
+                        check_session_key = 0
+                        for session_asc in SOCKET_SESSION:
+                            if sock == session_asc[0]:
+                                check_session_key = 1
+                                plaintext = decrypt_session(session_asc[1], data)
+                                msg = registration(plaintext)
+                                msg = encrypt_session(session_asc[1], msg)
+                                sock.send(msg)
+                        if check_session_key == 0:
+                            get_session_key(sock, privatekey, data)
                     else:  
                         if sock in SOCKET_LIST:
                             SOCKET_LIST.remove(sock)
@@ -63,6 +69,24 @@ def server():
 
     server_socket.close()
  
+def get_session_key(sock, privatekey, data):
+    cipherrsa = PKCS1_OAEP.new(privatekey)
+    sessionkey = cipherrsa.decrypt(data)
+    SOCKET_SESSION.append([sock, sessionkey])
+
+def decrypt_session(sessionkey, ciphertext):
+    iv = ciphertext[:16]
+    obj = AES.new(sessionkey, AES.MODE_CFB, iv)
+    plaintext = obj.decrypt(ciphertext)
+    plaintext = plaintext[16:]
+    return plaintext
+
+def encrypt_session(sessionkey, plaintext):
+    iv = Random.new().read(16) # 128 bit
+    obj = AES.new(sessionkey, AES.MODE_CFB, iv)
+    ciphertext = iv + obj.encrypt(plaintext)
+    return ciphertext
+
 def registration(data):
     login = data[:4]
 
@@ -72,13 +96,12 @@ def registration(data):
 
     password = data[4:10]
     uniq = authorization(login, password)
-    key = data[10:]
-    CLIENTS.append([login, uniq, key])
+    CLIENTS.append([login, uniq])
 
     return "Registration done:\n" + str(uniq)
 
 def authorization(login, password):
-    return int(login) + int(password)
+    return "lox-" + str(login)[:2] + str(password)[2:]
 
 if __name__ == "__main__":
     sys.exit(server())         
